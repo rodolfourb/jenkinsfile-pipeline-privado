417eaf9968d26a0157ebbd1876a9fa3ea06a8652
Rodolfo Urbano Rodrigues <rodolfourb@gmail.com>
2021-09-27 22:43:02 -0300

    Update pipeline-continuous-delivery.groovy

--- pipeline-continuous-delivery.groovy
+++ pipeline-continuous-delivery.groovy
@@ -17,7 +17,7 @@ pipeline {
             steps {
                 script {
                     git branch: 'master',
-                        url: 'https://github.com/teste/exemplo-spring-mvc-thymeleaf.git'
+                        url: 'https://github.com/rodolfourb/exemplo-spring-mvc-thymeleaf.git'
                 }
             }
         }
